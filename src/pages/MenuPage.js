be001b1ab80c66d0ee80adc067ee182f4bb85221
NJ-KIM11 <alwaysred1116@gmail.com>
2024-12-05 15:21:10 +0900

    asdfasf

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 
 
 const Menupage = () => {
+    const [menus, setMenus] = useState({});
+
+    useEffect(()=>{
+        fetch("https://www.beforeitcools.site:5555/menu/select")
+            .then(response=>{
+                console.log(response);
+                return response.json();
+            }).then(data => {
+                console.log(data);
+                setMenus(data);
+            }).catch(error => {
+                console.log("API 호출 중 오류 발생", error);
+            });
+    },[])
 
     return (
         <>
             <h1>우리가 메뉴를 불러와서 보여줄 페이지</h1>
+            <ul>{menus.map(function(item, index){
+                return <li>${item}</li>
+            })}
+            </ul>
         </>
     );
 };
